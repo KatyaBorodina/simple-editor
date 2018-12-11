d94188279e8493f03f221e797f1ba1c3d2ad72e0
Katya Borodina <katrisha27@gmail.com>
2018-12-11 23:50:07 +0200

    mothing works

--- src/js/resizable.js
+++ src/js/resizable.js
@@ -7,55 +7,65 @@ class Resizable extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            resizing: false
+        };
+
         this.el = React.createRef();
-        this.handleResizerMove = this.handleResizerMove.bind(this);
+
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
     }
 
     componentDidMount() {
-/*        let element = this.el.current;
-
-        element.addEventListener('mousedown', (event) => this.getInitialPosition(event));
-        element.addEventListener('mouseup', (event) => this.setNewPosition(event));*/
+        window.addEventListener('mousemove',this.handleMouseMove);
+        window.addEventListener('mouseup', this.handleMouseUp);
     }
 
     render() {
         return (
-            <div className="resizable" style={{ width: `${this.props.width + 2}em` }} ref={this.el}>
+            <div className="resizable" ref={this.el} style={{ width: `${this.props.width + 2}em` }}>
                 {this.props.children}
 
-                <div className="fn-resizer resizer top-left" onMouseMove={this.handleResizerMove}></div>
-                <div className="fn-resizer resizer top-right" onMouseMove={this.handleResizerMove}></div>
-                <div className="fn-resizer resizer bottom-right" onMouseMove={this.handleResizerMove}></div>
-                <div className="fn-resizer  resizer bottom-left" onMouseMove={this.handleResizerMove}></div>
+                <div className="fn-resizer resizer top-left"
+                     onMouseDown={this.handleMouseDown}
+                ></div>
+                <div className="fn-resizer resizer top-right"
+                     onMouseDown={this.handleMouseDown}
+                ></div>
+                <div className="fn-resizer resizer bottom-right"
+                     onMouseDown={this.handleMouseDown}
+                ></div>
+                <div className="fn-resizer resizer bottom-left"
+                     onMouseDown={this.handleMouseDown}
+                ></div>
             </div>
         )
     }
 
-    handleResizerMove(event) {
-        let newWidth = this.props.width + 1;
-
-        this.props.changeItemWidth(this.props.id, newWidth)
+    handleMouseUp() {
+        this.setState({
+            resizing: false
+        });
     }
 
-    getInitialPosition(event) {
-        if (!event.target.classList.contains('fn-resizer')) {
+    handleMouseDown(event) {
+        this.setState({
+            resizing: true
+        });
+    }
+
+    handleMouseMove(event) {
+        if (!this.state.resizing || !event.target.classList.contains('fn-resizer')) {
             return;
         }
 
-        window.addEventListener('mousemove', (event) => this.resizeElement(event));
-        window.addEventListener('mouseup', (event) => this.stopElementResizing(event));
-    }
+        console.log(event.clientX);
+        console.log(this.el.current.offsetLeft);
+        let newWidth = (event.clientX - this.el.current.offsetLeft) / 16;
 
-    setNewPosition(event) {
-
-    }
-
-    resizeElement(event) {
-
-    }
-
-    stopElementResizing(event) {
-
+        this.props.changeItemWidth(this.props.id, newWidth);
     }
 }
 
